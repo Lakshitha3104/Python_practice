cb5a1997fde5c7a93883c3861706d41ad3b6b26a
Lakshitha3104 <lakshita@lunamemarketingsolutions.in>
2026-05-13 12:21:54 +0530

    added

---
+++ student.py
@@ -0,0 +1,13 @@
+
+stu_name1 = input("Enter name1: ")
+stu_reg_no1 = int(input("Enter register number 1: "))
+stu_mark1 = int(input("Enter mark1: "))
+
+if stu_mark1 >= 35:
+        print (stu_name1 ,stu_reg_no1 ,stu_mark1 ,"Passed")
+else:
+        print(stu_name1 ,stu_reg_no1 ,stu_mark1 ,"Failed")
+    
+  
+
+
